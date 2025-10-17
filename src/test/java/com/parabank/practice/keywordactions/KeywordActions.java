18cb5adc57f194216dbfa815bc9f6d09634c4565
SIRAMU461 <ramuperfo@gmail.com>
2025-10-17 20:50:22 +0530

    javaScript Executor

--- src/test/java/com/parabank/practice/keywordactions/KeywordActions.java
+++ src/test/java/com/parabank/practice/keywordactions/KeywordActions.java
@@ -3,19 +3,21 @@ package com.parabank.practice.keywordactions;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class KeywordActions {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		KeywordActions action = new KeywordActions();
 
 		action.keywordActions();
 	}
 
-	public void keywordActions() {
+	public void keywordActions() throws InterruptedException {
 
 		WebDriver driver = new ChromeDriver();
 
@@ -34,8 +36,14 @@ public class KeywordActions {
 		Email.clear();
 
 		Email.sendKeys("ramuperfo@gmail.com");
-		
-	    
+
+		Thread.sleep(5000);
+
+		Actions act = new Actions(driver);
+
+		act.keyDown(Keys.CONTROL).sendKeys("A").keyUp(Keys.CONTROL).perform();
+		Thread.sleep(5000);
+		act.keyDown(Keys.CONTROL).sendKeys("X").keyUp(Keys.CONTROL).perform();
 
 	}
 
