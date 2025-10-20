de30e452e7248402edf24859c7635d2e44b52d4a
SIRAMU461 <ramuperfo@gmail.com>
2025-10-20 19:30:25 +0530

    Project name changes

--- src/test/java/com/parabank/practice/uploadfile/UploadFlie.java
+++ src/test/java/com/parabank/practice/uploadfile/UploadFlie.java
@@ -2,6 +2,7 @@ package com.parabank.practice.uploadfile;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class UploadFlie {
@@ -23,6 +24,17 @@ public class UploadFlie {
 		if(uploadfilename.equals("loan.txt")) {
 			System.out.println("Test case passed");
 		}
+		
+		
+		driver.close();
+		
+		WebDriver diver1 = new ChromeDriver();
+		
+		diver1.get("https://www.google.com");
+		
+		diver1.switchTo().newWindow(WindowType.TAB);
+		
+		diver1.get("https://www.google.com");
 
 	}
 
